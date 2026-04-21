c63e1e0744d14221194efb64aa5e532db6e0945c
Blackhero97 <your_email@example.com>
2026-04-21 17:12:13 +0500

    Initialize client-only repository

--- server/models/Setting.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const settingSchema = new mongoose.Schema({
-  passcode: { type: String, default: '2024' },
-}, { timestamps: true });
-
-const Setting = mongoose.model('Setting', settingSchema);
-export default Setting;
